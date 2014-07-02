f69f07b5a0273c4ece3fd45768f5c2e994d8fc89
Kevin Salchert <kevin.salchert@me.com>
2014-07-02 00:20:48 -0700

    Game objects now automatically reposition based on camera aspect ratio

---
+++ Assets/Scripts/AspectRatioReposition.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspectRatioReposition : MonoBehaviour
+{
+	[SerializeField] GameObject[] moveableObjects;
+
+	void Awake()
+	{
+		float aspectMultiplier = camera.aspect;
+
+		foreach (GameObject moveableObject in moveableObjects)
+		{
+			var modifiedPosition = moveableObject.transform.position;
+			modifiedPosition.x += modifiedPosition.x * (aspectMultiplier - (1024f/768f));
+			moveableObject.transform.position = modifiedPosition;
+		}
+	}
+}
