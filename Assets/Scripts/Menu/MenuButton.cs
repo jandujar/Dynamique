78c2783ee0f1ba8a581f921f2c9c6ab593a4121a
Kevin Salchert <kevin.salchert@me.com>
2014-07-25 11:39:31 -0700

    Started creating menu system

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuStateManager menuStateManager;
+
+	public enum ButtonState
+	{
+		Idle,
+		Unfold,
+		LevelSelect,
+		Options,
+	}
+	
+	public ButtonState buttonState;
+	
+	void OnEnable()
+	{
+		EasyTouch.On_TouchStart += On_TouchStart;
+	}
+
+	void OnDisable()
+	{
+		EasyTouch.On_TouchStart -= On_TouchStart;
+	}
+
+	void OnDestroy()
+	{
+		EasyTouch.On_TouchStart -= On_TouchStart;
+	}
+
+	public void On_TouchStart(Gesture gesture)
+	{
+		if (gesture.pickObject == gameObject)
+		{
+			switch(buttonState)
+			{
+			case ButtonState.Idle:
+				menuStateManager.menuState = MenuStateManager.MenuState.Idle;
+				break;
+			case ButtonState.Unfold:
+				menuStateManager.menuState = MenuStateManager.MenuState.Unfold;
+				break;
+			case ButtonState.LevelSelect:
+				menuStateManager.menuState = MenuStateManager.MenuState.LevelSelect;
+				break;
+			case ButtonState.Options:
+				menuStateManager.menuState = MenuStateManager.MenuState.Options;
+				break;
+			}
+
+			menuStateManager.SetState();
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/MenuStateManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStateManager : MonoBehaviour
+{
+	public enum MenuState
+	{
+		Idle,
+		Unfold,
+		LevelSelect,
+		Options
+	}
+	
+	public MenuState menuState;
+	
+	public void SetState()
+	{
+		switch(menuState)
+		{
+		case MenuState.Idle:
+			Idle();
+			break;
+		case MenuState.Unfold:
+			Unfold();
+			break;
+		case MenuState.LevelSelect:
+			LevelSelect();
+			break;
+		case MenuState.Options:
+			Options();
+			break;
+		}
+	}
+
+	void Idle()
+	{
+		Debug.Log("Idle");
+	}
+
+	void Unfold()
+	{
+		Debug.Log("Unfold");
+	}
+
+	void LevelSelect()
+	{
+		Debug.Log("Level Select");
+	}
+
+	void Options()
+	{
+		Debug.Log("Options");
+	}
+}
